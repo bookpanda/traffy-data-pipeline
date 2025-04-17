afe71af3c69b7b70de72567c8d5fb1880db4b682
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-18 00:38:40 +0700

    write to es

--- pipeline/spark/write.py
+++ pipeline/spark/write.py
@@ -17,6 +17,18 @@ def write_to_postgres(batch_df: DataFrame, batch_id):
 
 
 def write_to_elasticsearch(batch_df: DataFrame, epoch_id):
-    batch_df.write.format("org.elasticsearch.spark.sql").option(
-        "es.resource", "tickets/_doc"
-    ).option("es.nodes", "localhost").option("es.port", "9200").mode("append").save()
+    try:
+        (
+            batch_df.write.format("org.elasticsearch.spark.sql")
+            .option("es.resource", "tickets/_doc")
+            .option("es.nodes", "localhost")
+            .option("es.port", "9200")
+            .option("es.net.http.auth.user", "elastic")
+            .option("es.net.http.auth.pass", "elasticpassword")
+            .option("es.nodes.wan.only", "true")
+            .mode("append")
+            .save()
+        )
+    except Exception as e:
+        print(f"[ERROR] Failed to write to Elasticsearch: {e}")
+        raise
