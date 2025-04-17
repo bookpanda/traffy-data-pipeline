65f82be98869590c9815c66d06933b266ca15c5f
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-17 14:31:37 +0700

    str main

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,8 +1,12 @@
 from pipeline.kafka import main as kafka_main
 
+# from pipeline.stream import main as stream_main
+
 
 def main():
+
     kafka_main()
+    # stream_main()
 
 
 if __name__ == "__main__":
--- pipeline/stream/main.py
+++ pipeline/stream/main.py
@@ -1,1 +1,2 @@
-print("hi")
+def main():
+    print("stream")
